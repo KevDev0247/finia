e0c9316a867fd43c818724ce33083cfa565617b1
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-03-01 11:29:18 -0500

    Updates

--- app/src/main/java/protect/FinanceLord/ui/main/NetWorthFragment.java
+++ app/src/main/java/protect/FinanceLord/ui/main/NetWorthFragment.java
@@ -1,21 +1,29 @@
 package protect.FinanceLord.ui.main;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import protect.FinanceLord.R;
 
-public class NetWorthFragment extends Fragment {
+public class NetworthFragment extends Fragment {
     String title;
-    NetWorthFragment(String title){this.title = title;}
-
-    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+    NetworthFragment(String title) {
+        this.title = title;
+    }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_networth, null);
         TextView title = view.findViewById(R.id.item_number);
         title.setText(this.title);
