94745f2f84331411a05ee50fee40da5ba0f70e03
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-03-01 22:19:01 -0500

    updates net worth direct page

--- app/src/main/java/protect/FinanceLord/NetWorthActivity.java
+++ app/src/main/java/protect/FinanceLord/NetWorthActivity.java
@@ -8,14 +8,15 @@ import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 
+import protect.FinanceLord.ui.main.PastReportsAdapter;
+
 public class NetWorthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net_worth);
 
+        PastReportsAdapter pastReportsAdapter = new PastReportsAdapter(this);
 
     }
-
-
 }
---
+++ app/src/main/java/protect/FinanceLord/ui/main/PastReportsAdapter.java
@@ -0,0 +1,34 @@
+package protect.FinanceLord.ui.main;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class PastReportsAdapter extends BaseAdapter {
+    private final Context mContext;
+
+    public PastReportsAdapter(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
