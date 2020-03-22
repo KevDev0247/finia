19f4b17f568461bb9b203288b327f23803a4f9ad
ShileiMao <39761304+ShileiMao@users.noreply.github.com>
2020-03-22 23:11:00 +0000

    add toString to the query object

--- app/src/main/java/protect/FinanceLord/Database/AssetsTypeQuery.java
+++ app/src/main/java/protect/FinanceLord/Database/AssetsTypeQuery.java
@@ -6,4 +6,9 @@ public class AssetsTypeQuery {
     public String assetsSubType;
     public int thirdLevelId;
     public String assetsName;
+
+    public String toString() {
+        return "secondLevelId:" + secondLevelId + "\n" + "assetsCategory: " + assetsCategory + "\nassetsSubType: " + assetsSubType + "\n,thirdLevelId: " + thirdLevelId
+                 + "\nassetsName: " + assetsName;
+    }
 }
