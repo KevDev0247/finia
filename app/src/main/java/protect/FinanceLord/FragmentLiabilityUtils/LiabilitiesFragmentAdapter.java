7eb2267c67b781e840406b401622b7c862ab529a
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-04-17 19:24:52 -0400

    Liability Fragment update

--- app/src/main/java/protect/FinanceLord/FragmentLiabilityUtils/LiabilitiesFragmentAdapter.java
+++ app/src/main/java/protect/FinanceLord/FragmentLiabilityUtils/LiabilitiesFragmentAdapter.java
@@ -1,57 +1,80 @@
 package protect.FinanceLord.FragmentLiabilityUtils;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 
+import java.util.List;
+
 public class LiabilitiesFragmentAdapter extends BaseExpandableListAdapter {
-    @Override
-    public int getGroupCount() {
-        return 0;
+
+    private LiabilitiesFragmentDataProcessor dataProcessor;
+    private List<LiabilitiesFragmentDataCarrier> sectionDataSet;
+    private int level;
+    private Context context;
+
+    public LiabilitiesFragmentAdapter(LiabilitiesFragmentDataProcessor dataProcessor, List<LiabilitiesFragmentDataCarrier> sectionDataSet, int level, Context context){
+        this.dataProcessor = dataProcessor;
+        this.sectionDataSet = sectionDataSet;
+        this.level = level;
+        this.context = context;
+    }
+
+    public String getLiabilitiesName(int position){
+        return sectionDataSet.get(position).liabilitiesTypeName;
     }
 
     @Override
-    public int getChildrenCount(int i) {
-        return 0;
+    public int getGroupCount() {
+        return sectionDataSet.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i1) {
+        return 1;
     }
 
     @Override
     public Object getGroup(int i) {
-        return null;
+        return sectionDataSet.get(i);
     }
 
     @Override
     public Object getChild(int i, int i1) {
-        return null;
+
+        String liabilitiesTypeName = getLiabilitiesName(i);
+        List<LiabilitiesFragmentDataCarrier> carriers = dataProcessor.getGroupSet(liabilitiesTypeName, level + 1);
+        return carriers.get(i1);
     }
 
     @Override
     public long getGroupId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public long getChildId(int i, int i1) {
-        return 0;
+        return i1;
     }
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
 
     @Override
-    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+    public View getGroupView(int position, boolean b, View view, ViewGroup viewGroup) {
         return null;
     }
 
     @Override
-    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+    public View getChildView(int groupPosition, int childPosition, boolean b, View view, ViewGroup viewGroup) {
         return null;
     }
 
     @Override
     public boolean isChildSelectable(int i, int i1) {
-        return false;
+        return true;
     }
 }
