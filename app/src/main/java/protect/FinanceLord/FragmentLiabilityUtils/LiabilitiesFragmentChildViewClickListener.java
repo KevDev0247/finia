a763df7564446f5bd21cf7881d4d0d24dd90618e
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-04-17 19:25:31 -0400

    Liability click listener update

--- app/src/main/java/protect/FinanceLord/FragmentLiabilityUtils/LiabilitiesFragmentChildViewClickListener.java
+++ app/src/main/java/protect/FinanceLord/FragmentLiabilityUtils/LiabilitiesFragmentChildViewClickListener.java
@@ -1,4 +1,29 @@
 package protect.FinanceLord.FragmentLiabilityUtils;
 
-public class LiabilitiesFragmentChildViewClickListener {
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+
+import java.util.List;
+
+public class LiabilitiesFragmentChildViewClickListener implements ExpandableListView.OnChildClickListener {
+
+    private LiabilitiesFragmentDataProcessor dataProcessor;
+    private List<LiabilitiesFragmentDataCarrier> sectionDataSet;
+    private int level;
+
+    public LiabilitiesFragmentChildViewClickListener(LiabilitiesFragmentDataProcessor dataProcessor, List<LiabilitiesFragmentDataCarrier> sectionDataSet, int level){
+        this.dataProcessor = dataProcessor;
+        this.sectionDataSet = sectionDataSet;
+        this.level = level;
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+        LiabilitiesFragmentDataCarrier sectionItem = sectionDataSet.get(i);
+        List<LiabilitiesFragmentDataCarrier> childSection = dataProcessor.getGroupSet(sectionItem.liabilitiesTypeName, level + 1);
+
+        Log.d("LiabilitiesFragment", "child clicked: " + childSection.get(i1).liabilitiesTypeName + ", id in DB" + childSection.get(i1).liabilitiesId);
+        return false;
+    }
 }
