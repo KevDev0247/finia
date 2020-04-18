f95cb31df9ab2b6b5e413f649f779d2c11b326e3
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-04-18 11:47:38 -0400

    space

--- app/src/main/java/protect/FinanceLord/Database/AssetsType.java
+++ app/src/main/java/protect/FinanceLord/Database/AssetsType.java
@@ -35,4 +35,5 @@ public class AssetsType {
     public void setAssetsId(int assetsId)   {this.assetsId = assetsId;}
     public void setAssetsName(String assetsName)    {this.assetsName = assetsName;}
     public void setAssetsParentType(String assetsParentType)   {this.assetsParentType = assetsParentType;}
+
 }
