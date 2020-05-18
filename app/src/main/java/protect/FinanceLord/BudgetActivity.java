3e8f0cbd8446afc8580b76f825352d3843891834
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-05-18 16:19:06 -0400

    budgets UI updates

--- app/src/main/java/protect/FinanceLord/BudgetActivity.java
+++ app/src/main/java/protect/FinanceLord/BudgetActivity.java
@@ -1,8 +1,10 @@
 package protect.FinanceLord;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BudgetActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class BudgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+
+        resetView();
+
+
+    }
+
+    private void retrieveDataFromDatabase(){
+
+    }
+
+    private void resetView(){
+        ImageButton returnButton = findViewById(R.id.budget_return_button);
+        ImageButton addButton = findViewById(R.id.add_budget_button);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
