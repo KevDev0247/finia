2c6da9fb4be1cf2ed8a15da3aec9c1bcb6c4b676
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-05-20 18:13:02 -0400

    add saving action to Edit_Revenues

--- app/src/main/java/protect/FinanceLord/TransactionEditingUtils/Edit_RevenuesFragment.java
+++ app/src/main/java/protect/FinanceLord/TransactionEditingUtils/Edit_RevenuesFragment.java
@@ -13,11 +13,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Date;
+import java.util.concurrent.Executors;
 
 import protect.FinanceLord.Communicators.CalendarDateBroadcast;
 import protect.FinanceLord.Communicators.SaveDataCommunicator;
+import protect.FinanceLord.Database.FinanceLordDatabase;
+import protect.FinanceLord.Database.Transactions;
+import protect.FinanceLord.Database.TransactionsDao;
 import protect.FinanceLord.NetWorthReportEditingUtils.CalendarDialog;
 import protect.FinanceLord.NetWorthReportEditingUtils.NetWorthTimeUtils;
 import protect.FinanceLord.R;
@@ -26,11 +31,23 @@ import protect.FinanceLord.TransactionEditActivity;
 public class Edit_RevenuesFragment extends Fragment {
 
     Date currentTime;
+
+    private TextInputLayout nameInputField;
+    private TextInputLayout valueInputField;
+
     private TextInputEditText dateInput;
+    private TextInputEditText nameInput;
+    private TextInputEditText commentInput;
+    private TextInputEditText valueInput;
+
+    private static final String TAG = "Edit_RevenuesFragment";
+
     private SaveDataCommunicator fromActivityCommunicator = new SaveDataCommunicator() {
         @Override
         public void onActivityMessage() {
+            Log.d(TAG, "the message from activity was received");
 
+            retrieveDataFromInputBox();
         }
     };
 
@@ -45,10 +62,16 @@ public class Edit_RevenuesFragment extends Fragment {
         activity.toEditRevenuesFragmentCommunicator = fromActivityCommunicator;
     }
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View revenuesFragmentView = inflater.inflate(R.layout.fragment_edit_revenues, null);
+
+        nameInputField = revenuesFragmentView.findViewById(R.id.revenue_name_field);
+        valueInputField = revenuesFragmentView.findViewById(R.id.revenue_value_field);
+
+        nameInput = revenuesFragmentView.findViewById(R.id.revenue_name_input);
+        valueInput = revenuesFragmentView.findViewById(R.id.revenue_value_input);
+        commentInput = revenuesFragmentView.findViewById(R.id.revenue_comments_input);
         dateInput = revenuesFragmentView.findViewById(R.id.revenue_date_input);
 
         dateInput.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +79,7 @@ public class Edit_RevenuesFragment extends Fragment {
             public void onClick(View v) {
                 CalendarDialog calendarDialog = new CalendarDialog(calendarDialogCommunicator);
                 FragmentManager fragmentManager = getFragmentManager();
-                Log.d("Edit_RFragment", "Date input is clicked");
+                Log.d(TAG, "Date input is clicked");
                 calendarDialog.show(fragmentManager, "DateTimePicker");
             }
         });
@@ -68,9 +91,49 @@ public class Edit_RevenuesFragment extends Fragment {
         @Override
         public void onDialogMessage(Date date) {
             currentTime = date;
-            Log.d("Edit_RFragment", "time is " + currentTime);
+            Log.d(TAG, "time is " + currentTime);
             String stringDate = NetWorthTimeUtils.getStringFromDate(currentTime, getString(R.string.date_format));
             dateInput.setText(stringDate);
         }
     };
+
+    private void retrieveDataFromInputBox(){
+        Transactions transaction = new Transactions();
+
+        if (!nameInput.getText().toString().isEmpty()){
+            Log.d(TAG, "this transaction's name is " + nameInput.getText().toString());
+            transaction.setTransactionName(nameInput.getText().toString());
+        } else {
+            Log.d(TAG, "no data is inputted, an error should be displayed ");
+            nameInputField.setError(getString(R.string.transaction_name_error_message));
+        }
+
+        if (!valueInput.getText().toString().isEmpty()){
+            transaction.setTransactionValue(Float.parseFloat(valueInput.getText().toString().replace(",", "")));
+        } else {
+            valueInputField.setError(getString(R.string.transaction_value_error_message));
+        }
+
+        if (!commentInput.getText().toString().isEmpty()){
+            transaction.setTransactionComments(commentInput.getText().toString());
+        } else {
+            transaction.setTransactionComments(null);
+        }
+        transaction.setDate(currentTime.getTime());
+
+        insertTransactionIntoDb(transaction);
+    }
+
+    private void insertTransactionIntoDb(final Transactions transaction) {
+
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                FinanceLordDatabase database = FinanceLordDatabase.getInstance(getContext());
+                TransactionsDao transactionsDao = database.transactionsDao();
+
+                transactionsDao.insertTransaction(transaction);
+            }
+        });
+    }
 }
