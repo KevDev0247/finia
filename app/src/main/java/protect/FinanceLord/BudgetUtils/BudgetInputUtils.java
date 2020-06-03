01ffa9ce2e5e04c12d8e96fe57fb112dc5dc2e87
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-03 17:53:48 -0400

    Create budget utilities

---
+++ app/src/main/java/protect/FinanceLord/BudgetUtils/BudgetInputUtils.java
@@ -0,0 +1,23 @@
+package protect.FinanceLord.BudgetUtils;
+
+import android.widget.AutoCompleteTextView;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class BudgetInputUtils {
+    public TextInputLayout nameInputField;
+    public TextInputLayout valueInputField;
+    public TextInputLayout categoryInputField;
+    public TextInputLayout startDateInputField;
+    public TextInputLayout endDateInputField;
+
+    public TextInputEditText startDateInput;
+    public TextInputEditText endDateInput;
+    public TextInputEditText nameInput;
+    public TextInputEditText valueInput;
+    public AutoCompleteTextView categoryInput;
+
+    public RelativeLayout deleteButton;
+}
