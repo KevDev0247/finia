bfdc3cb5dd68c0388352869c212e947eab2a171d
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-07 15:09:06 -0400

    UI and font update

--- app/src/main/java/protect/FinanceLord/MainActivity.java
+++ app/src/main/java/protect/FinanceLord/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage_layout);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
