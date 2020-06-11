fdbe8f7895e9f1a704732daf2219a75b36370169
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-10 22:57:11 -0400

    Report item info javadoc comments

--- app/src/main/java/protect/FinanceLord/NetWorthPastReportsList/ReportItemInfo.java
+++ app/src/main/java/protect/FinanceLord/NetWorthPastReportsList/ReportItemInfo.java
@@ -1,5 +1,12 @@
 package protect.FinanceLord.NetWorthPastReportsList;
 
+/**
+ * The class to store important information of each report item.
+ * Report item refers to the item that displays the data and take the user to the report sheet.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public class ReportItemInfo {
     public int totalAssetsId;
     public float totalAssetsValue;
