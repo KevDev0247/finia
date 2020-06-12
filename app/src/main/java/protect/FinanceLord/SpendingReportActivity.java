fcdf0dce1674d2b46e0cd27c2c1f521b03feffa3
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-12 14:09:22 -0400

    Spending report list UI

---
+++ app/src/main/java/protect/FinanceLord/SpendingReportActivity.java
@@ -0,0 +1,14 @@
+package protect.FinanceLord;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpendingReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spending_report);
+    }
+}
