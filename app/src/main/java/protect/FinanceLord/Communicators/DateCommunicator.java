176e5e714dc49bce99d340150568f2665f686f0b
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-12 00:24:22 -0400

    Date communicator javadoc comments

--- app/src/main/java/protect/FinanceLord/Communicators/DateCommunicator.java
+++ app/src/main/java/protect/FinanceLord/Communicators/DateCommunicator.java
@@ -2,6 +2,15 @@ package protect.FinanceLord.Communicators;
 
 import java.util.Date;
 
+/**
+ * The communicator to communicate date between activity and fragments.
+ * This interface must be implemented by the activity receiving the message
+ * and the method message can only be called in the fragment that
+ * belongs to this activity.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public interface DateCommunicator {
     void message(Date date);
 }
