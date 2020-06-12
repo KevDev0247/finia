8d7907cd8f9873820fe394957b6e052da3888e89
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-12 16:34:00 -0400

    Set up Anychart view

--- app/src/main/java/protect/FinanceLord/SpendingReportActivity.java
+++ app/src/main/java/protect/FinanceLord/SpendingReportActivity.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.charts.Pie;
+
 public class SpendingReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spending_report);
+
+        AnyChartView spendingChartView = findViewById(R.id.spending_chart);
+
+        setUpPieChart(spendingChartView);
+    }
+
+    private void setUpPieChart(AnyChartView spendingChartView) {
+        Pie spendingChart = AnyChart.pie();
     }
 }
