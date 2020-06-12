ee971031bf95d2f7399b8872e32c2d897daff290
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-11 21:28:48 -0400

    Assets fragment adapter javadoc comments

--- app/src/main/java/protect/FinanceLord/NetWorthReportEditing/FragmentUtils/AssetsFragmentAdapter.java
+++ app/src/main/java/protect/FinanceLord/NetWorthReportEditing/FragmentUtils/AssetsFragmentAdapter.java
@@ -23,6 +23,18 @@ import protect.FinanceLord.NetWorthDataStructure.TypeTreeProcessor_Assets;
 import protect.FinanceLord.NetWorthReportEditing.NetWorthExpandableListView;
 import protect.FinanceLord.R;
 
+/**
+ * The adapter for the Edit_AssetsFragment.
+ * The adapter will carry the data from a source and deliver it to the UI widgets.
+ * The source refers to the data source in the value tree processor.
+ * Note that the carrier of the data in this adapter is the node carrier, which store the
+ * information about each node in the value tree and record the level of the node.
+ * This adapter will process the data in different level and deliver the group of data
+ * to the right parent and under the right level.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
 
     private ValueTreeProcessor_Assets valueTreeProcessor;
@@ -39,25 +51,56 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
         this.level = level;
     }
 
+    /**
+     * Get the current asset item's name
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param position the instance of commit button.
+     * @return name of the asset
+     */
     private String getAssetsName(int position) {
         return this.currentLevelNodesContainers.get(position).assetsTypeName;
     }
 
+    /**
+     * Get the count of the group of nodes.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return number of node containers in the group
+     */
     @Override
     public int getGroupCount() {
         return currentLevelNodesContainers.size();
     }
 
+    /**
+     * Gets the number of children in a specified group.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return number of node containers in the group
+     */
     @Override
     public int getChildrenCount(int i1) {
         return 1;
     }
 
+    /**
+     * Gets the data associated with the given group.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return the group selected
+     */
     @Override
     public Object getGroup(int i) {
         return currentLevelNodesContainers.get(i);
     }
 
+    /**
+     * Gets the current child node container of a parent node.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return current child node container of a parent node.
+     */
     @Override
     public Object getChild(int i, int i1) {
         String assetsTypeName = getAssetsName(i);
@@ -65,21 +108,55 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
         return carriers.get(i1);
     }
 
+    /**
+     * Gets the the id of the group.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return Id of the group.
+     */
     @Override
     public long getGroupId(int i) {
         return i;
     }
 
+    /**
+     * Gets the the id of the group.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return Id of the group.
+     */
     @Override
     public long getChildId(int i, int i1) {
         return i1;
     }
 
+    /**
+     * Indicates whether the child and group IDs are stable across changes to the underlying data.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @return Boolean.
+     */
     @Override
     public boolean hasStableIds() {
         return true;
     }
 
+    /**
+     * Create the view of a group.
+     * The method will determine the level of the node by checking the level of each node.
+     * Each item's data will be delivered to the right position with the help of processed data from the tree structure.
+     * Also, the input fields will be related to the right type using the processed data from the tree processor
+     * in order for the adapter to retrieve the right data for the right type.
+     * Then, a text listener will be set to retrieve the data from the input field and insert into the tree processor's data source
+     * to prepare for calculation and insertion into the database.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param position the position of the current item.
+     * @param b indicate whether the group is expanded.
+     * @param convertView the view object of the item.
+     * @param viewGroup the group of item view.
+     * @return View of a group.
+     */
     @Override
     public View getGroupView(int position, boolean b, View convertView, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -95,10 +172,10 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
             TextView textView = convertView.findViewById(R.id.assetsRowThirdText);
             textView.setText(this.currentLevelNodesContainers.get(position).assetsTypeName);
 
-            NodeContainer_Assets dataCarrier = this.currentLevelNodesContainers.get(position);
+            NodeContainer_Assets nodeContainer = this.currentLevelNodesContainers.get(position);
             EditText editText = convertView.findViewById(R.id.assetsValueInput);
 
-            AssetsValue assetsValue = valueTreeProcessor.getAssetValue(dataCarrier.assetsTypeId);
+            AssetsValue assetsValue = valueTreeProcessor.getAssetValue(nodeContainer.assetsTypeId);
 
             if (assetsValue != null) {
                 DecimalFormat decimalFormat = new DecimalFormat();
@@ -106,7 +183,7 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
                 editText.setText(strValue);
             }
 
-            this.addTextListener(editText, dataCarrier);
+            this.addTextListener(editText, nodeContainer);
 
         } else if (level == 2) {
             convertView = inflater.inflate(R.layout.assets_list_row_second_category, null);
@@ -133,6 +210,15 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    /**
+     * Retrieve the data from the input box and prepare for insertion.
+     * First, the method will detect the change in the input box record the value.
+     * Then, the method will reset the value the node in the tree, which is stored in processor data source.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param editText the input box for the current item.
+     * @param nodeContainer the node container that contain the information of each node.
+     */
     private void addTextListener(EditText editText, final NodeContainer_Assets nodeContainer) {
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -162,6 +248,17 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
         });
     }
 
+    /**
+     * Get the child view of the group.
+     * The method will set up the nested expandable list in a particular category item.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param groupPosition the position of the group.
+     * @param childPosition the position of the child item.
+     * @param isExpanded indicate whether the group is expanded.
+     * @param convertView the view of the child item
+     * @param parent group of parent items
+     */
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         final NodeContainer_Assets sectionData = currentLevelNodesContainers.get(groupPosition);
@@ -203,6 +300,13 @@ public class AssetsFragmentAdapter extends BaseExpandableListAdapter {
         }
     }
 
+    /**
+     * Determine whether the child at the specified position is selectable
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param i groupPosition
+     * @param i1 childPosition
+     */
     @Override
     public boolean isChildSelectable(int i, int i1) {
         return true;
