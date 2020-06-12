892c1f7b336f9565bf5bf59030546dc5764faf91
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-12 15:20:38 -0400

    Spending activity basic set up

--- app/src/main/java/protect/FinanceLord/SpendingActivity.java
+++ app/src/main/java/protect/FinanceLord/SpendingActivity.java
@@ -1,8 +1,14 @@
 package protect.FinanceLord;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.concurrent.Executors;
+
+import protect.FinanceLord.Database.FinanceLordDatabase;
 
 public class SpendingActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class SpendingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spending);
+
+        ImageButton returnButton = findViewById(R.id.spending_return_button);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void retrieveDataFromDatabase() {
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                FinanceLordDatabase database = FinanceLordDatabase.getInstance(SpendingActivity.this);
+
+            }
+        });
+    }
+
+    private void setUpSpendingList() {
+
     }
 }
