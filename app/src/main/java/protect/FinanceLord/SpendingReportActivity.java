918915d881f4cdfb061ff7ef33cad35dfa6743bd
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-14 19:42:02 -0400

    Spending report activity javadoc comments

--- app/src/main/java/protect/FinanceLord/SpendingReportActivity.java
+++ app/src/main/java/protect/FinanceLord/SpendingReportActivity.java
@@ -19,8 +19,26 @@ import java.util.List;
 import protect.FinanceLord.Database.BudgetsType;
 import protect.FinanceLord.SpendingUtils.GroupedSpending;
 
+/**
+ * The activity that allow the user to view the spending report.
+ * Each report contains a pie chart that display the distribution of spending in the current month.
+ * The user will be able to view the value of the category and its percentage in the total spending.
+ * The activity use a AnyChart library to implement the pie chart.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public class SpendingReportActivity extends AppCompatActivity {
 
+    /**
+     * Create and initialize the activity.
+     * This method was called when the activity was created.
+     * The method will first set the view of the content by finding the corresponding layout file through id.
+     * Then, it will initialize return button and set the page title to the date the user selected.
+     * Lastly, the method will call the method to set up the pie chart.
+     *
+     * @param savedInstanceState A mapping from String keys to various Parcelable values.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +57,22 @@ public class SpendingReportActivity extends AppCompatActivity {
         TextView pageTitle = findViewById(R.id.spending_report_page_title);
         pageTitle.setText(getIntent().getExtras().getString(getString(R.string.spending_report_month_key)));
 
-        AnyChartView spendingChartView = findViewById(R.id.spending_chart);
-
-        setUpPieChart(spendingChartView, allBudgetsTypes, groupedSpendingList, getIntent().getExtras().getString(getString(R.string.spending_report_month_key)));
+        setUpPieChart(allBudgetsTypes, groupedSpendingList, getIntent().getExtras().getString(getString(R.string.spending_report_month_key)));
     }
 
-    private void setUpPieChart(AnyChartView spendingChartView, List<BudgetsType> allBudgetsTypes, List<GroupedSpending> groupedSpendingList, String month) {
+    /**
+     * Set up the pie chart with the AnyChart library.
+     * The DataEntry class is used to store and format the data to generate the chart.
+     * First, the groupedSpendingList is traversed and the current month spending is retrieved.
+     * Then, the data stored in the current month spending is added into the data entries.
+     * Lastly, the data entry is added into the spendingChart to generate the chart
+     *
+     * @param allBudgetsTypes all budget types stored in the database.
+     * @param groupedSpendingList the class that store the more detailed data used to generate the report.
+     * @param month the month that the user selected.
+     */
+    private void setUpPieChart(List<BudgetsType> allBudgetsTypes, List<GroupedSpending> groupedSpendingList, String month) {
+        AnyChartView spendingChartView = findViewById(R.id.spending_chart);
         Pie spendingChart = AnyChart.pie();
         List<DataEntry> dataEntries = new ArrayList<>();
         List<GroupedSpending> currentMonthSpending = new ArrayList<>();
