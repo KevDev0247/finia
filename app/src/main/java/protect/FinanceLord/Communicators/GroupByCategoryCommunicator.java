1e263c29fb6acedbef5d583eb09d2a6c3a265d5e
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-14 17:43:58 -0400

    communicator javadoc comments

--- app/src/main/java/protect/FinanceLord/Communicators/GroupByCategoryCommunicator.java
+++ app/src/main/java/protect/FinanceLord/Communicators/GroupByCategoryCommunicator.java
@@ -1,5 +1,14 @@
 package protect.FinanceLord.Communicators;
 
+/**
+ * The communicator to communicate the filter the user clicked.
+ * This interface must be implemented by the activity receiving the message
+ * and the method message can only be called in the adapter that
+ * belongs to this activity.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public interface GroupByCategoryCommunicator {
     void message(String categoryLabel);
 }
