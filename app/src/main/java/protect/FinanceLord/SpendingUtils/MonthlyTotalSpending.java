34b8bc0a7762765146936a60fe874509759f1d14
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-14 19:47:34 -0400

    Monthly total spending javadoc comments

--- app/src/main/java/protect/FinanceLord/SpendingUtils/MonthlyTotalSpending.java
+++ app/src/main/java/protect/FinanceLord/SpendingUtils/MonthlyTotalSpending.java
@@ -1,5 +1,11 @@
 package protect.FinanceLord.SpendingUtils;
 
+/**
+ * The class to store the total spending and month for the report item in the spending report list.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
 public class MonthlyTotalSpending {
     public float categoryTotal;
     public String month;
