d2a1c83c2aa50e017e72a96e385500d85bbb8dfc
Kevcode-0247 <58012125+Kevcode-0247@users.noreply.github.com>
2020-06-14 15:52:09 -0400

    Category filters adapter javadoc comments

--- app/src/main/java/protect/FinanceLord/TransactionViewing/CategoryFiltersAdapter.java
+++ app/src/main/java/protect/FinanceLord/TransactionViewing/CategoryFiltersAdapter.java
@@ -16,7 +16,13 @@ import protect.FinanceLord.Communicators.GroupByCategoryCommunicator;
 import protect.FinanceLord.Database.BudgetsType;
 import protect.FinanceLord.R;
 
-public class CategoryFiltersAdapter extends RecyclerView.Adapter<CategoryFiltersAdapter.LabelViewHolder> {
+/**
+ * The recycler view adapter to deliver the data of each budget type name to the filter UI widget.
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * @version 2020.0609
+ */
+public class CategoryFiltersAdapter extends RecyclerView.Adapter<CategoryFiltersAdapter.FilterViewHolder> {
 
     private List<BudgetsType> budgetsTypes;
     private GroupByCategoryCommunicator toTransactionActivityCommunicator;
@@ -26,16 +32,31 @@ public class CategoryFiltersAdapter extends RecyclerView.Adapter<CategoryFilters
         this.toTransactionActivityCommunicator = fromAdapterCommunicator;
     }
 
+    /**
+     * Create the view holder of the filter item.
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param viewType the type of the view
+     * @param parent the ViewGroup of this list
+     */
     @NonNull
     @Override
-    public LabelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public FilterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.transaction_category_labels, parent, false);
 
-        return new LabelViewHolder(view);
+        return new FilterViewHolder(view);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     * @param holder the view holder of the filter
+     * @param position the position of the current item
+     */
     @Override
-    public void onBindViewHolder(@NonNull final LabelViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final FilterViewHolder holder, int position) {
         holder.categoryName.setText(budgetsTypes.get(position).getBudgetsName());
 
         holder.label.setOnClickListener(new View.OnClickListener() {
@@ -48,16 +69,26 @@ public class CategoryFiltersAdapter extends RecyclerView.Adapter<CategoryFilters
         });
     }
 
+    /**
+     * Get the number of items.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     */
     @Override
     public int getItemCount() {
         return budgetsTypes.size();
     }
 
-    static class LabelViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * The class of view holder for the filter of category types.
+     *
+     * @author Owner  Kevin Zhijun Wang
+     */
+    static class FilterViewHolder extends RecyclerView.ViewHolder {
         TextView categoryName;
         LinearLayout label;
 
-        LabelViewHolder(View itemView) {
+        FilterViewHolder(View itemView) {
             super(itemView);
             categoryName = itemView.findViewById(R.id.transaction_label_name);
             label = itemView.findViewById(R.id.transaction_category_label);
