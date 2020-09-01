4d4c7843fa9950488ff89f866afb73fc68c53673
KevDev0247 <donkevin1980s@gmail.com>
2020-08-31 23:48:19 -0400

    Add javadoc to AssetsValue

--- app/src/main/java/protect/Finia/Database/AssetsValue.java
+++ app/src/main/java/protect/Finia/Database/AssetsValue.java
@@ -5,8 +5,16 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
+/**
+ * The model for the database entity AssetsType
+ * AssetsType is used to store the value of each asset and the date they are recorded
+ *
+ * @author Owner  Kevin Zhijun Wang
+ * created on 2020/02/29
+ */
 @Entity(tableName = "AssetsValue")
 public class AssetsValue {
+
     @PrimaryKey(autoGenerate = true)
     private int assetsPrimaryId;
 
